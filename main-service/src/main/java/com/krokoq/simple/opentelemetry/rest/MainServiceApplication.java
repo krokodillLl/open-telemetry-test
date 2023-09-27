af5210ef4ff11f07581dfc35939fae8fdd4097d2
kirillsadrin <GtSpK35q>
2023-09-27 15:07:57 +0200

    added dependency for Azure monitor without Spring Boot starter into main-service

--- main-service/src/main/java/com/krokoq/simple/opentelemetry/rest/MainServiceApplication.java
+++ main-service/src/main/java/com/krokoq/simple/opentelemetry/rest/MainServiceApplication.java
@@ -1,5 +1,6 @@
 package com.krokoq.simple.opentelemetry.rest;
 
+import com.microsoft.applicationinsights.attach.ApplicationInsights;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MainServiceApplication {
 
 	public static void main(String[] args) {
+		ApplicationInsights.attach();
 		SpringApplication.run(MainServiceApplication.class, args);
 	}
 
